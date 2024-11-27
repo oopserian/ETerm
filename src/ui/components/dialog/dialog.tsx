d1d62456e962279aade1a81cf3e4bd1208804867
erian <oops.erian@gmail.com>
2024-11-27 10:36:53 +0800

    style:dialog

--- src/ui/components/dialog/dialog.tsx
+++ src/ui/components/dialog/dialog.tsx
@@ -14,13 +14,16 @@ const Dialog: React.FC<DialogProps> = ({ children, title, className, open = fals
     return (
         <div onClick={onClose} className={cn(
             'fixed w-full h-full top-0 left-0 bg-black/50 z-50 flex items-center justify-center',
-            'opacity-0 pointer-events-none',
+            'opacity-0 pointer-events-none transition-[opacity]',
             className,
             { 'opacity-100 pointer-events-auto': open })}>
-            <div onClick={(e) => e.stopPropagation()} className="bg-white rounded-xl w-full max-w-80 border border-zinc-200">
+            <div onClick={(e) => e.stopPropagation()} className={cn(
+                'rounded-xl w-full max-w-80 border border-zinc-200 bg-white shadow-sm',
+                'translate-y-4 transition-[transform]',
+                {'translate-y-0':open})}>
                 <div className="px-4 py-3 flex items-center justify-between border-b border-zinc-200">
-                    <p className="text-sm">{title}</p>
-                    <XMarkIcon onClick={onClose} className="w-4 h-4"></XMarkIcon>
+                    <p className="select-none text-sm">{title}</p>
+                    <XMarkIcon onClick={onClose} className="w-4 h-4 cursor-pointer"></XMarkIcon>
                 </div>
                 <div className="p-4 pt-3">
                     {children}
