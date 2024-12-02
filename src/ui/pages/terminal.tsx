c609c03223cd98f5bfb6e0a881cdc33e2c167ceb
erian <oops.erian@gmail.com>
2024-12-02 10:51:23 +0800

    style:调整终端

--- src/ui/pages/terminal.tsx
+++ src/ui/pages/terminal.tsx
@@ -1,14 +1,15 @@
 import '@xterm/xterm/css/xterm.css';
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import { Terminal } from "@xterm/xterm";
 import { FitAddon } from "@xterm/addon-fit";
 import { useParams } from 'react-router-dom';
 import useTerminalStore from '@/stores/useTerminalStore';
+import { ServerIcon } from '@heroicons/react/24/outline';
 
 export function Term() {
     return (
         <>
-            <div className="flex-1 w-full h-full py-1 pr-1 overflow-auto">
+            <div className="flex gap-2 flex-1 w-full h-full py-1 pr-1 overflow-auto">
                 <TerminalCom></TerminalCom>
             </div>
         </>
@@ -18,7 +19,9 @@ export function Term() {
 
 const TerminalCom: React.FC = () => {
     const { id } = useParams();
-    const { setCurTerminal } = useTerminalStore();
+    const {setCurTerminal,terminals} = useTerminalStore();
+    const terminalData = useMemo(() => terminals[id!], [terminals]);
+    if(!terminalData) return;
 
     const bgColor = '#212121';
     const terminalRef = useRef<HTMLDivElement | null>(null);
@@ -34,7 +37,7 @@ const TerminalCom: React.FC = () => {
 
         term.loadAddon(fitAddon);
         term.open(terminalRef.current!);
-        // fitAddon.fit();
+        fitAddon.fit();
 
         term.onData((command) => {
             window.terminal.input({
@@ -63,6 +66,12 @@ const TerminalCom: React.FC = () => {
     }, [id]);
 
     return (
-        <div ref={terminalRef} className="w-full h-full rounded-lg overflow-hidden p-2" style={{ background: bgColor }}></div>
+        <div className="flex flex-col gap-2 w-full h-full rounded-lg overflow-hidden p-2" style={{ background: bgColor }}>
+            <div className="flex items-center gap-2 text-white text-sm opacity-70">
+                <ServerIcon className="size-4" />
+                <p>{terminalData.host.alias}</p>
+            </div>
+            <div className="flex-1 bg-inherit [&_terminal]:h-full" ref={terminalRef}></div>
+        </div>
     )
 }
